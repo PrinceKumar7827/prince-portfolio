d9bb6336577b44998206fd8ec8f7ce9f94a88357
PrinceKumar7827 <princekumar7827312066@gmail.com>
2025-01-18 19:28:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function linker1() {
+    let a = document.getElementById("link1");
+    a.addEventListener("click", function() {
+        window.open("all projects/netflix-clone-main/clone.html", "_blank");
+    });
+}
+function linker2() {
+    let a = document.getElementById("link2");
+    a.addEventListener("click", function() {
+        window.open("all projects/spotify clone/home.html", "_blank");
+    });
+}
+function linker3() {
+    let a = document.getElementById("link3");
+    a.addEventListener("click", function() {
+        window.open("all projects/calculator/calculator.html", "_blank");
+    });
+}
+
+// function player() {
+//     let a = document.getElementById("vid");
+//     a.autopay = true;
+// }
