8ad0ac41eec91f1288ea761129df08f3dd7ed282
Prince <princekumar7827312066@gmail.com>
2025-04-21 14:31:10 +0530

    Delete all projects directory

--- all projects/spotify clone/home.js
+++
@@ -1,9 +0,0 @@
-
-function toggler() {
-    // let a = document.getElementById('input-field');
-    let dispCont = document.getElementById('disp');
-    window.location.href = '#';
-    dispCont.style.display = 'none';
-    let disp2 = document.getElementById('disp2');
-    disp2.style.display = 'block';
-}
